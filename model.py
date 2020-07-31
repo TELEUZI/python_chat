cc7f6ffda715ce6b1d291a71b5feed521a62de38
TELEUZI <41520794+TELEUZI@users.noreply.github.com>
2020-07-31 12:44:12 +0300

    Update model.py

--- model.py
+++ model.py
@@ -9,13 +9,13 @@ class Model():
     def __init__(self, controller):
         self.controller = controller
     def receiver(self):
-        self.massages = requests.get("https://a4d46fceca38.ngrok.io/send").json()["massages_for_receiving"]
+        self.massages = requests.get("HTTP://127.0.0.1:5000/send").json()["massages_for_receiving"]
         for massage in self.massages:
             self.controller.view.ui.massage_box.append(f'{massage["username"]}, {massage["text"]}, {massage["time"]}')
 
 
     def receive_new(self):
-        self.new_massages = requests.get("https://a4d46fceca38.ngrok.io/send").json()["massages_for_receiving"]
+        self.new_massages = requests.get("HTTP://127.0.0.1:5000/send").json()["massages_for_receiving"]
         for i in range(-len(self.new_massages) + len(self.massages), 0):
             self.controller.view.ui.massage_box.append(
                 f'{self.new_massages[i]["username"]}, {self.new_massages[i]["text"]}, {self.new_massages[i]["time"]}')
@@ -28,7 +28,7 @@ class Model():
 
     def send_new_massage(self):
         self.text = self.controller.view.ui.text_edit.toPlainText()
-        requests.post("https://a4d46fceca38.ngrok.io/send", json={"username": "username", "text": self.text, "time": time.ctime()})
+        requests.post("http://127.0.0.1:5000/send", json={"username": "username", "text": self.text, "time": time.ctime()})
         self.controller.view.ui.text_edit.setText("")
 
 
